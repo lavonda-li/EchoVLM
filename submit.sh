60f1c52062481c73c9a4797b8ec5d12f53a2fdce
lavonda-li <lavonda.li0531@gmail.com>
2024-12-12 20:50:43 -0800

    edited submit

--- submit.sh
+++ submit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=test
-#SBATCH --time=8:00:00
+#SBATCH --time=10:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=16G
+#SBATCH --mem-per-cpu=2G
 
 
 module purge
