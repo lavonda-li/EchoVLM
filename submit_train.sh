bea7c4781c75e9da5bfef23104cf3c6237a58558
lavonda-li <lavonda.li0531@gmail.com>
2025-01-07 20:59:41 -0800

    new sh

--- submit_train.sh
+++ submit_train.sh
@@ -8,4 +8,4 @@
 
 module purge
 module load python/3.12
-python3 ${HOME}/EchoVLM/process_captions.py --data_str train --batch_size 1000 --start_idx 79000
+python3 ${HOME}/EchoVLM/process_captions.py --data_str train --batch_size 1000 --start_idx 103000
